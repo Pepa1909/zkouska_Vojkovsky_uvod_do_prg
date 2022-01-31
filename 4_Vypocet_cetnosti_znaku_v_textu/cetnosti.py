d74d7bd5cbf48679688dd92a68d29703033de854
Josef Vojkovský <pepavoj@email.cz>
2022-01-31 16:34:00 +0100

    úprava proměnných

--- 4_Vypocet_cetnosti_znaku_v_textu/cetnosti.py
+++ 4_Vypocet_cetnosti_znaku_v_textu/cetnosti.py
@@ -1,16 +1,17 @@
 # Úvod
-print("Dobrý den, tento jednoduchý program umí vypočítat četnost všech znaků v zadaném textu.")
+print("Dobrý den, tento program umí vypočítat četnosti všech znaků v zadaném textu.")
 
-slovo = input("Zadejte text ke zpracování: ")
+text_input = str(input("Zadejte text ke zpracování: "))
 
 slovnik = {}
 
-# For cyklus projede každý znak v textu.
+# For cyklus kontroluje každý znak v textu.
 # Pokud znak ve slovníku je, započítá ho a pokud se znak obejví poprvé, vytvoří si ho jako novou hodnotu
-for znak in slovo:
+for znak in text_input:
     if znak in slovnik:
         slovnik[znak] += 1
     else:
         slovnik[znak] = 1
 
+print("V následujícím seznamu naleznete četnosti znaků v zadaném textu.")
 print(slovnik)
