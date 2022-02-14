f2b8fc7e367cbe035d34481eeac114836aa5fee8
Josef Vojkovský <pepavoj@email.cz>
2022-02-14 09:58:40 +0100

    Pillow

--- 85_Vizualizace_Kochovy_vlocky/koch.py
+++ 85_Vizualizace_Kochovy_vlocky/koch.py
@@ -1,33 +1,25 @@
-from turtle import forward, left, exitonclick, right, speed, setpos, up, down
+from PIL import Image, ImageDraw
+from math import sqrt
+def koch(x1, y1, x2, y2, n, draw):
+    if n > 0:
+#Strana 1
+        koch(x1, y1, x1 + (x2 - x1) / 3, y1 + (y2 - y1) / 3, n - 1, draw)
+#Strana2
+        koch(x1 + (x2 - x1) / 3, y1 + (y2 - y1) / 3, (x1 + x2) / 2 + (y2 - y1) / (2 * sqrt(3)), \
+(y1 + y2) / 2 + (x1 - x2) / (2 * sqrt(3)), n - 1, draw)
+#Strana 3
+        koch((x1 + x2) / 2 + (y2 - y1) / (2 * sqrt(3)), (y1 + y2) / 2 + (x1 - x2) / (2 * sqrt(3)), \
+        x1 + (x2 - x1) * 2 / 3, y1 + (y2 - y1) * 2 / 3, n - 1, draw)
+# #Strana 4
+        koch(x1 + 2 *(x2 - x1) / 3, y1 + 2 *(y2 - y1) / 3, x2, y2, n - 1, draw)
+    else:
+        draw.line((x1, y1, x2, y2), "black")
 
-def vlocka(uroven, delka): 
-
-    """Nakreslí Kochovu vločku zadané úrovně a zadaného rozměru."""
-
-    if uroven == 0:
-        forward(delka)
-        return
-    vlocka(uroven-1,delka/3)
-    left(60)
-    vlocka(uroven-1,delka/3)
-    right(120)
-    vlocka(uroven-1,delka/3)
-    left(60)
-    vlocka(uroven-1,delka/3)
-
-print("Dobrý den, tento program nakreslí Kochovu vločku. Úroveň i velikost si budete moci zvolit.")
-
-uroven = int(input("Zadejte úroveň Kochovy vločky, kterou chcete vykreslit (úroveň vyšší než 4 již trvá dlouho): "))
-velikost = float(input("Zadejte velikost Kochovy vločky (doporučená velikost je 200-500): "))
-
-# Změna rychlosti, aby se vločka kreslila co nejrychleji a také změna výchozích souřadnic, aby větší vločky nebyly mimo monitor
-speed(0)
-up()
-setpos(-200,100)
-down()
-
-# For cyklus zaručující, že vzniklý tvar bude opravdu vločka
-for _ in range(3):
-    vlocka(uroven,velikost)
-    right(120)
-exitonclick()
+im = Image.new("RGB", (800, 650), color="white")
+draw = ImageDraw.Draw(im)
+d = 550
+h = 1
+koch(0 , 160, d, 160, h, draw)
+# koch(100 + d, 160, 100 + 0.5 * d, 160 + d * sqrt(3) / 2, h, draw)
+# koch(100 + 0.5 * d, 160 + d * sqrt(3) / 2, 100, 160, h, draw)
+im.show()
