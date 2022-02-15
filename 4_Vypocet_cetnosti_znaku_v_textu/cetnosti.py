5da6d1171e4ebbacf7b7bf56b47a46cf95844673
Josef Vojkovský <pepavoj@email.cz>
2022-02-15 18:21:30 +0100

    komentáře, úprava technických zpráv

--- 4_Vypocet_cetnosti_znaku_v_textu/cetnosti.py
+++ 4_Vypocet_cetnosti_znaku_v_textu/cetnosti.py
@@ -1,21 +1,23 @@
-from pydoc import text
-
-
 def counts():
+
+    """Counts the number of unique characters in the input text; returns a dictionary"""
+
     adict = {}
-    text = str(input("Zadejte text ke zpracování: ")).lower()
+    input_text = str(input("Zadejte text ke zpracování: ")).lower()
+
     # For loop iterates over every character in input text
     # If a character already is a key in the dictionary, add 1 to value; if it's a new character, make a new key and set its value to 1
-    for char in text:
+    for char in input_text:
         if char in adict:
             adict[char] += 1
         else:
             adict[char] = 1
+
+    print("V následujícím slovníku naleznete četnosti znaků v zadaném textu:")
     return adict
 
 # Starting info
 print("Dobrý den, tento program umí vypočítat četnosti všech znaků v zadaném textu.")
 
-# Result
-print("V následujícím seznamu naleznete četnosti znaků v zadaném textu:")
+# Main application
 print(counts())
--- 85_Vizualizace_Kochovy_vlocky/koch.py
+++ 85_Vizualizace_Kochovy_vlocky/koch.py
@@ -1,25 +1,37 @@
 from PIL import Image, ImageDraw
 from math import sqrt
-def koch(x1, y1, x2, y2, n, draw):
-    if n > 0:
-#Strana 1
-        koch(x1, y1, x1 + (x2 - x1) / 3, y1 + (y2 - y1) / 3, n - 1, draw)
-#Strana2
+
+# Main function, that uses recursion to draw every part of the snwoflake
+def koch(x1, y1, x2, y2, number_of_iterations, draw):
+
+    """Draws a Koch snowflake with a number of desired iterations"""
+    # Recursion ending condition
+    if number_of_iterations > 0:
+# Side 1
+        koch(x1, y1, x1 + (x2 - x1) / 3, y1 + (y2 - y1) / 3, number_of_iterations - 1, draw)
+# Side 2
         koch(x1 + (x2 - x1) / 3, y1 + (y2 - y1) / 3, (x1 + x2) / 2 + (y2 - y1) / (2 * sqrt(3)), \
-(y1 + y2) / 2 + (x1 - x2) / (2 * sqrt(3)), n - 1, draw)
-#Strana 3
+        (y1 + y2) / 2 + (x1 - x2) / (2 * sqrt(3)), number_of_iterations - 1, draw)
+# Side 3
         koch((x1 + x2) / 2 + (y2 - y1) / (2 * sqrt(3)), (y1 + y2) / 2 + (x1 - x2) / (2 * sqrt(3)), \
-        x1 + (x2 - x1) * 2 / 3, y1 + (y2 - y1) * 2 / 3, n - 1, draw)
-# #Strana 4
-        koch(x1 + 2 *(x2 - x1) / 3, y1 + 2 *(y2 - y1) / 3, x2, y2, n - 1, draw)
+        x1 + (x2 - x1) * 2 / 3, y1 + (y2 - y1) * 2 / 3, number_of_iterations - 1, draw)
+# Side 4
+        koch(x1 + 2 *(x2 - x1) / 3, y1 + 2 *(y2 - y1) / 3, x2, y2, number_of_iterations - 1, draw)
+    # If the number of iterations reaches zero, a line is drawn with current parameters
     else:
         draw.line((x1, y1, x2, y2), "black")
 
 im = Image.new("RGB", (800, 650), color="white")
 draw = ImageDraw.Draw(im)
-d = 550
-h = 1
-koch(0 , 160, d, 160, h, draw)
-# koch(100 + d, 160, 100 + 0.5 * d, 160 + d * sqrt(3) / 2, h, draw)
-# koch(100 + 0.5 * d, 160 + d * sqrt(3) / 2, 100, 160, h, draw)
+length = 550
+iterations = int(input("Zvolte úroveň Kochovy vločky (doporučená maximální úroveň je 6): "))
+
+# Draws the upper side of the snowflake
+koch(100, 160, length + 100, 160, iterations, draw)
+
+# Draws the right side of the snowflake
+koch(100 + length, 160, length/2 + 100, 160 + length * sqrt(3) / 2, iterations, draw)
+
+# Draws the left side of the snowflake
+koch(100 + 0.5 * length, 160 + length * sqrt(3) / 2, 100, 160, iterations, draw)
 im.show()
